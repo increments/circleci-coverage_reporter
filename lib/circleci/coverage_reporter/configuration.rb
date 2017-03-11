e2df133e2c8308022d857485b3068bf42b782b02
Yuku TAKAHASHI <taka84u9@gmail.com>
2017-03-12 01:04:03 +0900

    Add some default configuration

--- lib/circleci/coverage_reporter/configuration.rb
+++ lib/circleci/coverage_reporter/configuration.rb
@@ -6,10 +6,9 @@ module CircleCI
       DEFAULT_REPORTERS = [SimpleCov::Reporter.new].freeze
       DEFAULT_VCS_TYPE = 'github'.freeze
 
-      attr_accessor :artifacts_dir, :base_revision, :circleci_token, :current_revision, :previous_build_number,
-                    :repository_name, :user_name, :vcs_token
-
-      attr_writer :reporters, :vcs_type
+      attr_accessor :circleci_token, :vcs_token
+      attr_writer :artifacts_dir, :base_revision, :current_revision, :previous_build_number, :reporters,
+                  :repository_name, :user_name, :vcs_type
 
       # @return [String]
       def project
@@ -24,6 +23,36 @@ module CircleCI
       # @return [String]
       def vcs_type
         @vcs_type ||= DEFAULT_VCS_TYPE
+      end
+
+      # @return [String]
+      def artifacts_dir
+        @artifacts_dir ||= ENV['CIRCLE_ARTIFACTS']
+      end
+
+      # @return [String]
+      def base_revision
+        @base_revision ||= `git merge-base origin/master HEAD`.strip
+      end
+
+      # @return [String]
+      def current_revision
+        @current_revision ||= ENV['CIRCLE_SHA1']
+      end
+
+      # @return [Integer]
+      def previous_build_number
+        @previous_build_number ||= ENV['CIRCLE_PREVIOUS_BUILD_NUM'].to_i
+      end
+
+      # @return [String]
+      def repository_name
+        @repository_name ||= ENV['CIRCLE_PROJECT_REPONAME']
+      end
+
+      # @return [String]
+      def user_name
+        @user_name ||= ENV['CIRCLE_PROJECT_USERNAME']
       end
     end
   end
--- lib/circleci/coverage_reporter/rake_task.rb
+++ lib/circleci/coverage_reporter/rake_task.rb
@@ -8,13 +8,6 @@ namespace :circleci do
     CircleCI::CoverageReporter.configure do |config|
       config.circleci_token = ENV['COVERAGE_REPORTER_CIRCLECI_TOKEN']
       config.vcs_token = ENV['COVERAGE_REPORTER_VCS_TOKEN']
-
-      config.artifacts_dir = ENV['CIRCLE_ARTIFACTS']
-      config.base_revision = `git merge-base origin/master HEAD`.strip
-      config.current_revision = ENV['CIRCLE_SHA1']
-      config.previous_build_number = ENV['CIRCLE_PREVIOUS_BUILD_NUM'].to_i
-      config.repository_name = ENV['CIRCLE_PROJECT_REPONAME']
-      config.user_name = ENV['CIRCLE_PROJECT_USERNAME']
     end
 
     CircleCI::CoverageReporter.run
